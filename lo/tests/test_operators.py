dfb16b501da148dcbbdccc8c7bbca3d9df7799df
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-08-25 16:38:49 +0200

    Add tests files for iterative and operators

---
+++ lo/tests/test_operators.py
@@ -0,0 +1,13 @@
+"""Test functions for the lo.operators module
+"""
+
+from numpy.testing import *
+
+import numpy as np
+import scipy.sparse as sparse
+
+from lo.operators import *
+
+
+if __name__ == "__main__":
+    nose.run(argv=['', __file__])
