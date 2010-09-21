99340f34175604a449082d5d42682fcb16bc830a
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-21 12:42:12 +0200

    corrected bug in cudamat_lo

--- lo/wrappers/cudamat_lo.py
+++ lo/wrappers/cudamat_lo.py
@@ -6,13 +6,18 @@ import numpy as np
 import lo
 
 def aslinearoperator(a):
+    from cudamat import CUDAMatrix
     if isinstance(a, np.ndarray):
-        a_gpu = cudamat.CUDAMatrix(a)
+        a_gpu = CUDAMatrix(a)
     elif isinstance(a, cudamat.CUDAMatrix):
         a_gpu = a
     else:
         raise ValueError('Expected CUDAMatrix or ndarray')
     # define linear operator
-    matvec = lambda x: cudamat.dot(a, x)
-    rmatvec = lambda x: cudamat.dot(a.transpose(), x)
+    def matvec(x):
+        x_gpu = CUDAMatrix(x.reshape((x.size, 1)))
+        return cudamat.dot(a_gpu, x_gpu).asarray()
+    def rmatvec(x):
+        x_gpu = CUDAMatrix(x.reshape((x.size, 1)))
+        return cudamat.dot(a_gpu.transpose(), x_gpu).asarray()
     return lo.LinearOperator(a.shape, matvec, rmatvec, dtype=a.dtype)
