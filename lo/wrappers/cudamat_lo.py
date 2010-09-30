e7be4654a71744402627caa40470c21a6c181b59
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-09-30 15:56:35 +0200

    allow to get and pass CUDAMatrix instead of ndarrays

--- lo/wrappers/cudamat_lo.py
+++ lo/wrappers/cudamat_lo.py
@@ -9,17 +9,29 @@ import lo
 def cudamat_aslinearoperator(a):
     if isinstance(a, np.ndarray):
         a_gpu = CUDAMatrix(a)
-    elif isinstance(a, cudamat.CUDAMatrix):
+    elif isinstance(a, CUDAMatrix):
         a_gpu = a
     else:
         raise ValueError('Expected CUDAMatrix or ndarray')
     # define linear operator
     def matvec(x):
-        x.resize((x.size, 1))
-        x_gpu = CUDAMatrix(x)
-        return cudamat.dot(a_gpu, x_gpu).asarray()
+        if isinstance(x, np.ndarray):
+            x.resize((x.size, 1))
+            x_gpu = CUDAMatrix(x)
+            return cudamat.dot(a_gpu, x_gpu).asarray()
+        elif isinstance(x, CUDAMatrix):
+            x_gpu = x
+            return cudamat.dot(a_gpu, x_gpu)
+        else:
+            raise ValueError('Expected CUDAMatrix or ndarray')
     def rmatvec(x):
-        x.resize((x.size, 1))
-        x_gpu = CUDAMatrix(x)
-        return cudamat.dot(a_gpu.transpose(), x_gpu).asarray()
+        if isinstance(x, np.ndarray):
+            x.resize((x.size, 1))
+            x_gpu = CUDAMatrix(x)
+            return cudamat.dot(a_gpu.transpose(), x_gpu).asarray()
+        elif isinstance(x, CUDAMatrix):
+            x_gpu = x
+            return cudamat.dot(a_gpu.transpose(), x_gpu)
+        else:
+            raise ValueError('Expected CUDAMatrix or ndarray')
     return lo.LinearOperator(a.shape, matvec, rmatvec, dtype=a.dtype)
