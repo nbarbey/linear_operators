76b54fdd4759988eda616f57772fef54f61e75fd
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-01-13 18:22:03 +0100

    Utils module with condition number estimation.

---
+++ lo/iterative/utils.py
@@ -0,0 +1,19 @@
+"""
+Usefull functions for LinearOperators
+"""
+import numpy as np
+
+def cond(A, k=1, symmetric=True, M=None, maxiter=None, tol=0):
+    """
+    Find the condition number of a LinearOperator using arpack eigen
+    function.
+    """
+    if symmetric:
+        from scipy.sparse.linalg import eigen_symmetric as eigen
+    else:
+        from scipy.sparse.linalg import eigen
+
+    smax = np.max(eigen(A, which='LM', k=k, M=M, maxiter=maxiter, tol=tol, return_eigenvectors=False))
+    smin = np.min(eigen(A, which='SA', k=k, M=M, maxiter=maxiter, tol=tol, return_eigenvectors=False))
+    
+    return smax / smin
