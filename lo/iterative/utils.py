aa922bb1f07e41f28a89bcf34abdfd10bec24d79
Nicolas Barbey <nbarbey@sapherschel9.extra.cea.fr>
2011-02-03 14:37:51 +0100

    Improved condition number estimator.

--- lo/iterative/utils.py
+++ lo/iterative/utils.py
@@ -2,18 +2,47 @@
 Usefull functions for LinearOperators
 """
 import numpy as np
+try:
+    from scipy.sparse.linalg import arpack
+except(ImportError):
+    import arpack
 
-def cond(A, k=1, symmetric=True, M=None, maxiter=None, tol=0):
+def cond(A, k=2, kl=None, ks=None, symmetric=True, M=None, maxiter=None,
+         tol=1e-6, verbose=False, prune_zeros=False, loweig=None):
     """
     Find the condition number of a LinearOperator using arpack eigen
     function.
     """
-    if symmetric:
-        from scipy.sparse.linalg.arpack import eigen_symmetric as eigen
-    else:
-        from scipy.sparse.linalg.arpack import eigen
+    if kl is None:
+        kl = k
+    if ks is None:
+        ks = k
 
-    smax = np.max(eigen(A, which='LM', k=k, M=M, maxiter=maxiter, tol=tol, return_eigenvectors=False))
-    smin = np.min(eigen(A, which='SA', k=k, M=M, maxiter=maxiter, tol=tol, return_eigenvectors=False))
-    
+    if symmetric:
+        eigen = arpack.eigen_symmetric
+    else:
+        eigen = arpack.eigen
+
+    vmax = eigen(A, which='LM', k=kl, M=M, maxiter=maxiter, tol=tol, return_eigenvectors=False)
+    vmin = eigen(A, which='SM', k=ks, M=M, maxiter=maxiter, tol=tol, return_eigenvectors=False)
+    nb_null = np.sum(vmin == 0)
+    if verbose:
+        print("Found %d zero-valued eigenvalues" % nb_null)
+        if nb_null == len(vmin):
+            print "All small eigenvalues were zeros."
+    if prune_zeros:
+        vmin = vmin[vmin !=0]
+    if vmin.size == 0:
+        return np.inf
+    smin = np.min(vmin)
+    smax = np.max(vmax)
+    if smin == 0:
+        return np.inf
+    if loweig is not None:
+        if smin < loweig:
+            return np.inf
+    if verbose:
+        print vmin, vmax
+        print smin, smax
+
     return smax / smin
