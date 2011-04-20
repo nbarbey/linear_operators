bd0d99e79c0cf150469aac47690f7f20e0dd3392
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-04-20 13:44:30 +0200

    Extended tests for fft operators

--- tests/test_ndoperators.py
+++ tests/test_ndoperators.py
@@ -19,6 +19,7 @@ def generate_permutation(n):
 
 # collection of linear operators to test
 shapein16 = (16, 16)
+shapein3_4 = (4, 4, 4)
 d = np.random.rand(*shapein16)
 coef = np.random.uniform(high=10.)
 mask = np.random.rand(*shapein16) > .5
@@ -29,9 +30,10 @@ id16 = op.ndidentity(shapein16)
 d16 = op.nddiagonal(d)
 h16 = op.ndhomothetic(shapein16, coef)
 m16 = op.ndmask(mask)
-f16 = op.fft2(shapein16, dtype=np.complex128)
+f1_16 = op.fft(shapein16, dtype=np.complex128)
+f2_16 = op.fft2(shapein16, dtype=np.complex128)
 
-lo_list = [id16, d16, h16, m16, f16]
+lo_list = [id16, d16, h16, m16, f1_16, f2_16]
 
 # collection of vectors
 ones16 = np.ones(shapein16)
@@ -40,7 +42,7 @@ rand16 = np.random.rand(*shapein16)
 
 v_list = [ones16, zeros16, rand16 ]
 
-# tests
+# general tests
 def check_matvec(A, x):
     A * x.ravel()
 
@@ -65,6 +67,35 @@ def check_dense_transpose(A):
 def test_dense_transpose():
     for A in lo_list:
         yield check_dense_transpose, A
+
+# FFT tests
+def check_almost_dense_transpose(A):
+    Ad = A.todense()
+    Adt = A.T.todense()
+    assert_array_almost_equal(np.conj(Ad.T), Adt)
+
+def test_fft_n_complex():
+    for n in xrange(1, shapein16[-1]):
+        A = op.fft(shapein16, n=n, dtype=np.complex128)
+        yield check_almost_dense_transpose, A
+
+def test_fft_n_real():
+    for n in xrange(1, shapein16[-1]):
+        A = op.fft(shapein16, n=n, dtype=np.float64)
+        yield check_almost_dense_transpose, A
+
+def test_fft2_s_complex():
+    for n1 in xrange(1, shapein16[0]):
+        for n2 in xrange(1, shapein16[1]):
+            s = (n1, n2)
+            A = op.fft2(shapein16, s=s, dtype=np.complex128)
+            yield check_almost_dense_transpose, A
+
+def test_fft3_s_complex():
+    for n1 in xrange(1, shapein3_4[0]):
+        s = (n1, n1, n1)
+        A = op.fftn(shapein3_4, s=s, dtype=np.complex128)
+        yield check_almost_dense_transpose, A
 
 if __name__ == "__main__":
     nose.run(argv=['', __file__])
