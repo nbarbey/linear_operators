71cd47fe3c5db9ada7fd565d1493d769440d0ce8
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-04-29 17:00:53 +0200

    Add docstring describing lo

--- lo/__init__.py
+++ lo/__init__.py
@@ -1,5 +1,35 @@
-from iterative import *
+"""
+lo is a package implementing estimation algorithms for large scale
+linear problems. The workflow is as follows : generate a linear model
+using LinearOperator instances as buildling blocks, define a criterion
+to minimize using this model, and finally perform minimization on the
+criterion using a minimization algorithm.
+
+Subpackages implements each part of this workflow. Here is a list of
+the subpackages :
+
+- interface : Taken from the scipy.sparse package. It implement the
+  LinearOperator class which replaces matrices and do not require to
+  store any matrix coefficient. It makes use of matrix-vector
+  operations instead.
+
+- operators : A set of LinearOperator subclasses implementing various
+   linear operation on vectors and their transpose.
+
+- ndoperators : A set of LinarOperator subclasses implementing
+  operations on multidimensional arrays.
+
+- iterative : Contains the Criterion class which allows to define objective
+  function as well as minimizers such as the conjugate gradient algorithms
+  and wrappers to other minimizing algorithms.
+
+- wrappers : define extra LinearOperator subclasses if optional
+  dependencies are available.
+
+"""
+
 from interface import *
+from iterative import *
 from operators import *
 from ndoperators import *
 from wrappers import *
