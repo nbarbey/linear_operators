b6b55068af3b74ea6b395d5431b1a11d9d10c8a3
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-05-03 11:21:50 +0200

    Add docstrings to operators classes

--- linear_operators/operators.py
+++ linear_operators/operators.py
@@ -1,4 +1,41 @@
-"""Definition of useful linear operators"""
+"""Definition of specialized LinearOperator subclasses.
+
+Classes
+-------
+
+- SymmetricOperator : Enforces matvec = rmatvec and square shape.
+
+- IdentityOperator : Identity operator.
+
+- HomotheticOperator : Homothetic operators : i.e. of the form a * I.
+
+- DiagonalOperator : Diagonal operator.
+
+- MaskOperator : Diagonal operator with ones and zeros on the diagonal.
+
+- ShiftOperator : Shift the input vector values.
+
+- PermutationOperator : Perform permutations of the input vector values.
+
+- FftOperator : Perform one-dimensional Fast-Fourier Transform (np.fft.fft)
+
+- ReplicationOperator : Replicates the input vector n times.
+
+Functions
+---------
+
+Functions generate instances of the associated classes. The following are available :
+
+- identity
+- homothetic
+- diagonal
+- mask
+- shift
+- permutation
+- fft
+- replication
+
+"""
 import numpy as np
 from copy import copy
 from interface import LinearOperator, concatenate
@@ -6,7 +43,27 @@ from interface import LinearOperator, concatenate
 # subclasses operators
 
 class SymmetricOperator(LinearOperator):
+    """
+    Subclass of LinearOperator for the definition of symmetric
+    operators, i.e. operators such that : M^T == M.  It only requires
+    a shape and a matvec function, since the rmatvec should be the
+    same function as matvec.
+    """
     def __init__(self, shape, matvec, **kwargs):
+        """
+        Parameters
+        ----------
+
+        shape : length 2 tuple
+            The shape of the operator. Should be square.
+
+        matvec : function
+            The matrix-vector operation.
+
+        Returns
+        -------
+        A SymmetricOperator instance.
+        """
         if shape[0] != shape[1]:
             raise ValueError("Symmetric operators are square operators.")
         kwargs['rmatvec'] = matvec
@@ -14,11 +71,56 @@ class SymmetricOperator(LinearOperator):
 
 class IdentityOperator(SymmetricOperator):
     def __init__(self, shape, **kwargs):
+        """
+        Parameters
+        ----------
+
+        shape : length 2 tuple
+            The shape of the operator. Should be square.
+
+        Returns
+        -------
+        An IdentityOperator instance.
+
+        Exemple
+        -------
+        >>> import linear_operators as lo
+        >>> I = lo.IdentityOperator((3, 3))
+        >>> I.todense()
+        array([[ 1.,  0.,  0.],
+               [ 0.,  1.,  0.],
+               [ 0.,  0.,  1.]])
+
+        """
         matvec = lambda x: x
         SymmetricOperator.__init__(self, shape, matvec, **kwargs)
 
 class HomotheticOperator(SymmetricOperator):
     def __init__(self, shape, coef, **kwargs):
+        """
+        Parameters
+        ----------
+
+        shape : length 2 tuple
+            The shape of the operator. Should be square.
+
+        coef : int or float
+            The multiplication coefficient.
+
+        Returns
+        -------
+        An HomotheticOperator instance.
+
+        Exemple
+        -------
+        >>> import linear_operators as lo
+        >>> H = lo.HomotheticOperator((3, 3), 2.)
+        >>> H.todense()
+        array([[ 2.,  0.,  0.],
+               [ 0.,  2.,  0.],
+               [ 0.,  0.,  2.]])
+
+        """
         self.coef = coef
         matvec = lambda x: coef * x
         SymmetricOperator.__init__(self, shape, matvec, **kwargs)
@@ -28,6 +130,26 @@ class HomotheticOperator(SymmetricOperator):
 
 class DiagonalOperator(SymmetricOperator):
     def __init__(self, d, **kwargs):
+        """
+        Parameters
+        ----------
+        d : ndarray with ndim == 1.
+            The diagonal of the operator.
+
+        Returns
+        -------
+        A DiagonalOperator instance.
+
+        Exemple
+        -------
+        >>> import numpy as np
+        >>> import linear_operators as lo
+        >>> D = lo.DiagonalOperator(np.arange(3))
+        >>> D.todense()
+        array([[ 0.,  0.,  0.],
+               [ 0.,  1.,  0.],
+               [ 0.,  0.,  2.]])
+        """
         shape = 2 * (d.size,)
         self.d = d
         matvec = lambda x: x * d
@@ -38,6 +160,29 @@ class DiagonalOperator(SymmetricOperator):
 
 class MaskOperator(DiagonalOperator):
     def __init__(self, mask, **kwargs):
+        """
+        Parameters
+        ----------
+        mask : ndarray of ones and zeros.
+            If mask[i] = 0, the corresponding value will be masked.
+            If mask is not a boolean array, it is converted to boolean.
+
+        Returns
+        -------
+        A MaskOperator instance.
+
+        Exemple
+        -------
+        >>> import numpy as np
+        >>> import linear_operators as lo
+        >>> M = lo.MaskOperator(np.arange(4) % 2)
+        >>> M.todense()
+        array([[ 0.,  0.,  0.,  0.],
+               [ 0.,  1.,  0.,  0.],
+               [ 0.,  0.,  0.,  0.],
+               [ 0.,  0.,  0.,  1.]])
+
+        """
         self.mask = mask.astype(np.bool)
         DiagonalOperator.__init__(self, self.mask, **kwargs)
 
@@ -67,6 +212,29 @@ class ShiftOperator(LinearOperator):
 
 class PermutationOperator(LinearOperator):
     def __init__(self, p, **kwargs):
+        """
+        Parameters
+        ----------
+        p : list or tuple
+            The permutation of coefficients.
+
+        Returns
+        -------
+        A PermutationOperator instance.
+
+        Exemple
+        -------
+        >>> import numpy as np
+        >>> import linear_operators as lo
+        >>> P = lo.PermutationOperator([3, 1, 0, 2])
+        >>> P.todense()
+        array([[ 0.,  0.,  0.,  1.],
+               [ 0.,  1.,  0.,  0.],
+               [ 1.,  0.,  0.,  0.],
+               [ 0.,  0.,  1.,  0.]])
+
+        """
+
         shape = 2 * (len(p),)
         self.p = p
         # reverse permutation
@@ -87,10 +255,31 @@ class FftOperator(LinearOperator):
         LinearOperator.__init__(self, shape, matvec, rmatvec=rmatvec, **kwargs)
 
 class ReplicationOperator(LinearOperator):
-    """
-    Output n times the input
-    """
     def __init__(self, shape, n, **kwargs):
+        """
+        Parameters
+        ----------
+        shape : length 2 tuple.
+            The shape of the operator.
+
+        n : int
+            The number of replications.
+
+        Returns
+        -------
+        A ReplicationOperator instance.
+
+        Exemple
+        -------
+        >>> import numpy as np
+        >>> import linear_operators as lo
+        >>> R = lo.ReplicationOperator((4, 2), 2)
+        >>> R.todense()
+        array([[ 1.,  0.],
+               [ 0.,  1.],
+               [ 1.,  0.],
+               [ 0.,  1.]])
+        """
         self.n = n
         # ensure coherent input
         if not shape[0] == shape[1] * n:
