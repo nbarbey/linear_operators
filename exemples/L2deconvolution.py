024f3eea9e32f6e404aae5466b50d9c29abd6f0c
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-05-16 12:32:05 +0200

    A simple L2deconvolution exemple;

---
+++ exemples/L2deconvolution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import numpy as np
+import scipy
+import linear_operators as lo
+
+# Load the infamous Lena image from scipy
+im = scipy.lena()
+# Generate a convolution model with a 3x3 uniform kernel
+kernel = np.ones((3, 3))
+model = lo.convolve_ndimage(im.shape, kernel)
+# convolve the original image
+data = model(im)
+# add noise to the convolved data
+noise = 1e1 * np.random.randn(*data.shape)
+data += noise
+# define smoothness priors
+prior = [lo.diff(im.shape, axis=i) for i in xrange(im.ndim)]
+hypers = (1e1, 1e1)
+# generate an conjugate gradient algorithm from model, data and priors
+algo = lo.QuadraticConjugateGradient(model, data, prior, hypers)
+# start the estimation algorithm
+xe = algo()
+# reshape the output as the algorithm only handles vectors
+xe.resize(im.shape)
