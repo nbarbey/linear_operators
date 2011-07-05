9c4d9b51f8f4af4824790e004dba46f79cad266b
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-07-05 12:13:12 +0200

    Cleaned up exemple

--- exemples/deconvolution_dli.py
+++ exemples/deconvolution_dli.py
@@ -7,18 +7,17 @@ import linear_operators as lo
 im = scipy.lena()
 im = im[::4, ::4]
 # Generate a convolution model with a 7x7 uniform kernel
-model = lo.convolve_ndimage(im.shape, np.ones((7, 7)))
+model = lo.convolve_fftw3(im.shape, np.ones((7, 7)))
 # convolve the original image
 data = model * im.ravel()
 # add noise to the convolved data
 data += 1e0 * np.random.randn(*data.shape)
 # define smoothness prior
-prior = lo.concatenate([lo.diff(im.shape, axis=i) for i in xrange(im.ndim)])
+#prior = lo.concatenate([lo.diff(im.shape, axis=i) for i in xrange(im.ndim)])
+prior = lo.concatenate([lo.diff(im.shape, axis=i) for i in xrange(im.ndim)]
+                       + [lo.wavelet2(im.shape, "haar"),])
 # generate algorithm
-algo = lo.DoubleLoopAlgorithm(model, data, prior,
-                              lanczos={"maxiter":100},
-                              optimizer=lo.FminNCG,
-                              stop_condition=lo.StopCondition(maxiter=5))
+algo = lo.DoubleLoopAlgorithm(model, data, prior)
 # start the estimation algorithm
 xe = algo()
 # reshape the output as the algorithm only handles vectors
